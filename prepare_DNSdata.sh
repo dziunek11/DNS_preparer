cced74d10535e029166114a52de0857bf2c90b9d
dziunek11 <68402425+dziunek11@users.noreply.github.com>
2024-10-18 22:56:07 -0400

    Update prepare_DNSdata.sh
    nrp uncommented and echo improved

--- prepare_DNSdata.sh
+++ prepare_DNSdata.sh
@@ -25,34 +25,35 @@ echo ''
 echo 'Cactus downloaded'
 echo ''
 
-#git clone https://github.com/zachetienne/nrpytutorial.git
-#cd nrpytutorial/IllinoisGRMHD/doc
-#./generate_IllinoisGRMHD_from_ipynb_files.sh
-#cd ../../../Cactus/arrangements/WVUThorns
-#rm IllinoisGRMHD
-#rm Convert_to_HydroBase
-#rm ID_converter_ILGRMHD
-#ln -s ../../../nrpytutorial/IllinoisGRMHD/ IllinoisGRMHD
-#ln -s ../../../nrpytutorial/IllinoisGRMHD/Convert_to_HydroBase Convert_to_HydroBase
-#ln -s ../../../nrpytutorial/IllinoisGRMHD/ID_converter_ILGRMHD ID_converter_ILGRMHD
-#cd ../../../../
+git clone https://github.com/zachetienne/nrpytutorial.git
+cd nrpytutorial/IllinoisGRMHD/doc
+./generate_IllinoisGRMHD_from_ipynb_files.sh
+cd ../../../Cactus/arrangements/WVUThorns
+rm IllinoisGRMHD
+rm Convert_to_HydroBase
+rm ID_converter_ILGRMHD
+ln -s ../../../nrpytutorial/IllinoisGRMHD/ IllinoisGRMHD
+ln -s ../../../nrpytutorial/IllinoisGRMHD/Convert_to_HydroBase Convert_to_HydroBase
+ln -s ../../../nrpytutorial/IllinoisGRMHD/ID_converter_ILGRMHD ID_converter_ILGRMHD
+cd ../../../../
 
-#echo ''
-#echo 'nrp downloaded and linked'
-#echo 'All done'
-#echo 'Things to do now'
-#echo ''
+echo ''
+echo 'nrp downloaded and linked'
+echo 'All done'
+echo ''
+echo 'Things to do now'
+echo ''
 #echo '1. Go to the Sgrid directory and try to compile it by typing "make". If failed, you must properly set MyConfog and try again.'
 #echo ''
 #echo '2. Set the right path to the Sgrid library in your dns.cfg file. It probably supposed to be'
 #echo 'LIBDIRS = path_to_this_project/sgrid/lib, and then the library itself:'
 #echo 'LIBS = sgrid'
 #echo ''
-#echo '3. Go to Cactus directory and compile it using ll_comp or by typing:'
-#echo './simfactory/bin/sim setup-silent'
-#echo './simfactory/bin/sim build -j24 --thornlist ../dns.th --optionlist ../dns.cfg'
+echo '1. Go to Cactus directory and compile it using ll_comp or by typing:'
+echo './simfactory/bin/sim setup-silent'
+echo './simfactory/bin/sim build -j24 --thornlist ../dns.th --optionlist ../dns.cfg'
 #echo ''
-#echo '4. Set the right path to the Sgrid-generated initial data directory in dns.par. and build the skeleton of the run by typing:'
+echo '2. Set the right path to the initial data directory in yout parfile.'
 #echo './simfactory/bin/sim create-run name_of_the_run --parfile ../dns.par'
 #echo ''
 #echo '5. See Cactus/arrangements/CactusSgrid/DNSdata/README for details'
